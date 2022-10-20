cc9f1fe89edf1b0923546c42e381b73f8fdac3aa
Xareth <sareth2277@gmail.com>
2022-10-20 21:38:13 +0200

    Add AC and DEL functions

--- script.js
+++ script.js
@@ -1,7 +1,75 @@
 // ==========================================================================
-// Etch-A-Sketch
+// Neon Calculator
 // ==========================================================================
+
+// ==========================================================================
+// TOC
+// ==========================================================================
+
+// ==========================================================================
+// DOM References
+// ==========================================================================
+
+const currentNum = document.querySelector('.currentNum');
+const numbers = document.querySelectorAll('.number');
+const ac = document.querySelector('#ac');
+const del = document.querySelector('#del');
 
 // ==========================================================================
 // Functions
 // ==========================================================================
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(operator, a, b) {
+    return operator(a, b);
+};
+
+function allClear() {
+    displayValue = 0;
+    currentNum.textContent = displayValue;
+};
+
+function deleteNum() {
+    if (displayValue.length <= 1) {
+        displayValue = 0;
+    } else {
+        displayValue = displayValue.slice(0, -1);
+    }
+    currentNum.textContent = displayValue;
+};
+
+function storeValue(value) {
+    console.log(value);
+    displayValue = displayValue + value;
+    currentNum.textContent = displayValue;
+};
+
+// ==========================================================================
+// Event-Listeners
+// ==========================================================================
+
+ac.addEventListener('click', allClear);
+
+del.addEventListener('click', deleteNum);
+
+numbers.forEach(number => number.addEventListener('click', () => {
+    storeValue(number.value);
+}));
+
+let displayValue = '';
+
